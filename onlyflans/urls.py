a076702afe01858bed7402efa635d72516039af4
jaime-godoy <jaimegodoy1990@gmail.com>
2024-04-17 21:03:57 -0400

    HITO 3

--- onlyflans/urls.py
+++ onlyflans/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web.views import index,about,welcome
+from web.views import index,about,welcome,contact,success
 
 
 urlpatterns = [
@@ -24,7 +24,8 @@ urlpatterns = [
     path('', index, name="index"),
     path('about/', about, name="about"),
     path('welcome/', welcome, name="welcome"),
-    
+    path('contact/', contact, name="contact"),
+    path('success/', success, name="success"),
 
 
 ]
